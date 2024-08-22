dd31e97418ed69607a92814fcc844068fb4c12a1
Lucas Sena <106786101+LucasSena-dev@users.noreply.github.com>
2024-08-22 18:44:09 -0300

    update

--- ScreenSoundPOO/Program.cs
+++ ScreenSoundPOO/Program.cs
@@ -4,14 +4,13 @@ musica1.artista = "The police";
 musica1.duracao = 273;
 musica1.disponivel = true;
 
-musica1.ExibirFichaTecnica();
-
 Musica musica2 = new Musica();
 musica2.nome = "Vertigo";
 musica2.artista = "U2";
 musica2.duracao = 367;
 musica2.disponivel = false;
 
+musica1.ExibirFichaTecnica();
 musica2.ExibirFichaTecnica();
 
 
