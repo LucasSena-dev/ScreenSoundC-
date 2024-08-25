568e56d1b9edc93cc7d3f7301b8dc75f04262716
Lucas Sena <106786101+LucasSena-dev@users.noreply.github.com>
2024-08-25 15:44:57 -0300

    lambda

--- ScreenSoundPOO/Musica.cs
+++ ScreenSoundPOO/Musica.cs
@@ -1,14 +1,16 @@
 class Musica{
-    public String nome;
-    public String artista;
-    public double duracao;
-    public bool disponivel;
+    public string Nome { get; set; }
+    public string Artista { get; set; }
+    public double Duracao { get; set; }
+    public bool Disponivel { get ; set; } //propriedade com PascalCase
+    public string DescricaoResumida  => $"A música {Nome} pertence a banda {Artista}"; // => lambda 
+         // prop tab que só existe o get (leitura) a parte de escrita (set) nao tem = {get;}
 
     public void ExibirFichaTecnica(){
-        Console.WriteLine($"Nome: {nome}");
-        Console.WriteLine($"Artista: {artista}");
-        Console.WriteLine($"Duração: {duracao}");
-        if (disponivel){
+        Console.WriteLine($"Nome: {Nome}");
+        Console.WriteLine($"Artista: {Artista}");
+        Console.WriteLine($"Duração: {Duracao}");
+        if (Disponivel){
             Console.WriteLine("Música está disponível no plano.");
         }
         else {
